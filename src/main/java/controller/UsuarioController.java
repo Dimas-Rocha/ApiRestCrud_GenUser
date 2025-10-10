cf5fcddad0df96866f6e1bc9a330b8bfab5033ad
Dimas Rocha <dimas.dasp@gmail.com>
2025-10-09 23:56:26 -0300

    controller

--- src/main/java/controller/UsuarioController.java
+++ src/main/java/controller/UsuarioController.java
@@ -1,5 +1,97 @@
 package controller;
 
-public class UsuarioController {
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import model.Usuario;
+import service.UsuarioService;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@CrossOrigin(origins = "*") // Permite requests de qualquer origem (para testes)
+public class UsuarioController {
+    
+    @Autowired
+    private UsuarioService usuarioService;
+    
+    // POST - Criar novo usuário
+    @PostMapping
+    public ResponseEntity<Usuario> criarUsuario(@RequestBody Usuario usuario) {
+        try {
+            Usuario novoUsuario = usuarioService.criarUsuario(usuario);
+            return new ResponseEntity<>(novoUsuario, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    // GET - Buscar todos os usuários
+    @GetMapping
+    public ResponseEntity<List<Usuario>> buscarTodosUsuarios() {
+        try {
+            List<Usuario> usuarios = usuarioService.buscarTodosUsuarios();
+            
+            if (usuarios.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            
+            return new ResponseEntity<>(usuarios, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    // GET - Buscar usuário por ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> buscarUsuarioPorId(@PathVariable Long id) {
+        Optional<Usuario> usuario = usuarioService.buscarUsuarioPorId(id);
+        
+        if (usuario.isPresent()) {
+            return new ResponseEntity<>(usuario.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    
+    // PUT - Atualizar usuário
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> atualizarUsuario(@PathVariable Long id, @RequestBody Usuario usuario) {
+        Usuario usuarioAtualizado = usuarioService.atualizarUsuario(id, usuario);
+        
+        if (usuarioAtualizado != null) {
+            return new ResponseEntity<>(usuarioAtualizado, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    
+    // DELETE - Deletar usuário
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deletarUsuario(@PathVariable Long id) {
+        try {
+            boolean deletado = usuarioService.deletarUsuario(id);
+            
+            if (deletado) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
